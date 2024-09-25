116c3a7741a0a498a3112bba6b3501660a51939b
Matthew T. Iwane <mattiwane@gmail.com>
2024-09-24 21:35:11 -0700

    setting up react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,24 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
   return (
-    <>
-      <h1>App Component</h1>
-    </>
+    <BrowserRouter>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+      <main>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,9 @@
+const root = () => {
+  return (
+    <div>
+        <h1>Root Component</h1>
+    </div>
+  )
+}
+
+export default root
