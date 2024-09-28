9fd100e5108922eca1305e4e46191e58e8305de9
iwane, matthew <mattiwane@gmail.com>
2024-09-28 00:25:39 -0400

    finished projects sections version 1

--- frontend/src/components/home/projects/ProjectSection.jsx
+++ frontend/src/components/home/projects/ProjectSection.jsx
@@ -1,6 +1,21 @@
 import "./module.ProjectSection.css";
 import data from "../../../data/data.json";
 
+// Project Images
+import bu_course_search from "../../../assets/projects/bu_course_search/bu-course-search.png"
+import exercise_tracker from "../../../assets/projects/exercise_tracker/exercise-tracker.png"
+import art_book_collective from "../../../assets/projects/spark/art-book-collective.png"
+import little_lemon from "../../../assets/projects/little_lemon/little_lemon_hero.png"
+
+import coming_soon from "../../../assets/projects/coming_soon.jpg"
+
+const getImageSrc = {
+  "Boston University Course Search" : bu_course_search,
+  "Exercise Tracker" : exercise_tracker,
+  "Art Book Collective" : art_book_collective,
+  "Little Lemon" : little_lemon
+}
+
 const ProjectSection = () => {
   return (
     <div className="project-container">
@@ -8,7 +23,7 @@ const ProjectSection = () => {
       <div className="cards-container">
         {data.projects.map((p) => (
           <div className="card" key={p.id}>
-            {<img className="card-image" src={p.image} alt="card image" />}
+            {<img className="card-image" src={getImageSrc[p.project_name] ? getImageSrc[p.project_name] :  coming_soon} alt="card image" />}
             {<div className="card-name">{p.project_name}</div>}
             {
               <div className="tags-container">
