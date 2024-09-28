9c0e0c137a63acd25dfb7714bb80eb876e791ce5
Matthew T. Iwane <mattiwane@gmail.com>
2024-09-27 17:09:25 -0700

    fixed project grid columns and reorganized directory

--- frontend/src/components/home/projects/ProjectSection.jsx
+++ frontend/src/components/home/projects/ProjectSection.jsx
@@ -1,20 +1,17 @@
 import "./module.ProjectSection.css";
 import data from "../../../data/data.json";
 
-import img from "../../../assets/project-images/spark/art-book-collective.png";
+// import img from "../../../assets/projects/spark/art-book-collective.png";
 
 const ProjectSection = () => {
   return (
     <div className="project-container">
       <h2 id="projects">Projects</h2>
 
-      <hr />
-      <br />
-
       <div className="cards-container">
         {data.projects.map((p) => (
           <div className="card" key={p.id}>
-            {<img className="card-image" src={img} alt="card image" />}
+            {<img className="card-image" src={p.image} alt="card image" />}
             {<div className="card-name">{p.project_name}</div>}
             {
               <div className="tags-container">
