f0b55dfca941dfbae1c447c13812d819428aed53
iwane, matthew <mattiwane@gmail.com>
2024-10-13 23:50:25 -0700

    re-added firebase config to show portfolio section

--- frontend/firebase.config.js
+++ frontend/firebase.config.js
@@ -11,6 +11,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-// const app = initializeApp(firebaseConfig);
-// export const storage = getStorage(app, import.meta.env.VITE_STORAGE_URL);
-// export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app, import.meta.env.VITE_STORAGE_URL);
+export const db = getFirestore(app);
