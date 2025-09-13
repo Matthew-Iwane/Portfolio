bdce67592ecf4536889ac65c1abcdfcf32f18d63
Matthew <mattiwane@gmail.com>
2025-09-12 23:40:04 -0700

    added ei5 to my portfolio

--- frontend/src/components/home/portfolio/projects/ProjectSection.jsx
+++ frontend/src/components/home/portfolio/projects/ProjectSection.jsx
@@ -8,6 +8,7 @@ import "./module.ProjectSection.css";
 import Loader from "../../../shared/loader/Loader";
 
 // Project Images
+import ei5 from "../../../../assets/projects/ei5/ei5.png"
 import bu_course_search from "../../../../assets/projects/bu_course_search/bu-course-search.png"
 import exercise_tracker from "../../../../assets/projects/exercise_tracker/exercise-tracker.png"
 import art_book_collective from "../../../../assets/projects/spark/art-book-collective.png"
@@ -17,11 +18,13 @@ import job_board from "../../../../assets/projects/job_board/jobboard.png"
 import coming_soon from "../../../../assets/projects/coming_soon.jpg"
 
 const getImageSrc = {
+  "ei5.jp" : ei5,
   "Boston University Course Search" : bu_course_search,
   "Exercise Tracker" : exercise_tracker,
   "Art Book Collective" : art_book_collective,
-  "Little Lemon" : little_lemon,
   "Job Board" : job_board,
+  "Little Lemon" : little_lemon,
+
 }
 
 const LinkVariants = {
