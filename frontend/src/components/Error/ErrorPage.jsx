8ebe812c19a81d233c15b375b3199e16fc8ee260
Matthew <mattiwane@gmail.com>
2025-09-30 22:31:00 -0700

    fixed the error page to be a separate component and applied minor text changes

--- frontend/src/components/Error/ErrorPage.jsx
+++ frontend/src/components/Error/ErrorPage.jsx
@@ -1,5 +1,5 @@
-import "../components/error/module.ErrorPage.css"
-import sad_gif from "../assets/gifs/sad.gif"
+import "./module.ErrorPage.css"
+import sad_gif from "../../assets/gifs/sad.gif"
 // import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
@@ -8,7 +8,7 @@ export default function ErrorPage() {
 
   return (
     <div className="error-page">
-      <h1>Oops!</h1>
+      <h1>Uh oh!</h1>
       <p>This page does <span>not</span> exist... check the <span>navigation menu</span> for valid <span>links</span>!</p>
       <img src={sad_gif} alt="crying marshmallow" id="sad-marshmallow"/>
 
---
+++ frontend/src/pages/Error.jsx
@@ -0,0 +1,8 @@
+import ErrorPage from "../components/Error/ErrorPage";
+
+export default function Error() {
+
+  return (
+    <ErrorPage />
+  );
+}
